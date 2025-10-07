b07a56e7206973b43ef541125d2222f5f103ebf7
Allan Mwenja <mwenjaallan18@gmail.com>
2025-10-07 10:34:22 +0300

    Envirornment variable

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 8000
 const app = express();
 
 // setup static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+let posts = [
+    {id:1, title: 'Post One'},
+    {id:2, title: 'Post Two'},
+    {id:3, title: 'Post Three'},
+];
+
+// Get all posts
+app.get('/api/post', (req, res) => {
+    res.json(posts);
+})
+
+app.listen(port, () => console.log(`Server is running on port ${8000}`));
