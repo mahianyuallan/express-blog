0108ca268a185aff2116aff6d7c345a845387e1b
Allan Mwenja <mwenjaallan18@gmail.com>
2025-10-07 11:39:03 +0300

    Route files and ES modules

--- server.js
+++ server.js
@@ -1,42 +1,16 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8000
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js'
+const port = process.env.PORT || 8000;
+
 const app = express();
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
+app.use('/api/posts', posts);
 
-let posts = [
-    {id:1, title: 'Post One'},
-    {id:2, title: 'Post Two'},
-    {id:3, title: 'Post Three'},
-];
 
-// Get all posts
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit);
-
-    if(!isNaN(limit) && limit > 0){
-        res.status(200).json(posts.slice(0, limit));
-    } else {
-        res.status(200).json(posts);
-    }
-    
-});
-
-// Get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.find((post)=> post.id === id);
-
-    if(!post) {
-        return res
-        .status(404)
-        .json({ msg: `A post with the id of ${id} was not found`})
-    }
-        res.status(200).json(post);
-    
-});
 
 app.listen(port, () => console.log(`Server is running on port ${8000}`));
