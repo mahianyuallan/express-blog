e27ba2a47b113848164039e69b43a7e0fbe90fda
Allan Mwenja <mwenjaallan18@gmail.com>
2025-10-07 11:18:46 +0300

    Query Strings and Setting Status codes

--- server.js
+++ server.js
@@ -15,13 +15,28 @@ let posts = [
 
 // Get all posts
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0){
+        res.status(200).json(posts.slice(0, limit));
+    } else {
+        res.status(200).json(posts);
+    }
+    
 });
 
 // Get single post
 app.get('/api/posts/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    res.json(posts.filter((post) => post.id === id));
+    const post = posts.find((post)=> post.id === id);
+
+    if(!post) {
+        return res
+        .status(404)
+        .json({ msg: `A post with the id of ${id} was not found`})
+    }
+        res.status(200).json(post);
+    
 });
 
 app.listen(port, () => console.log(`Server is running on port ${8000}`));
