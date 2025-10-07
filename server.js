23414eec2326595d7b65011c49381069b2daa0f8
Allan Mwenja <mwenjaallan18@gmail.com>
2025-10-07 10:14:32 +0300

    Watch flag & NPM scripts

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello World' });
+    res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About');
 });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
