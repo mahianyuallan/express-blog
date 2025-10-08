5291ee81d5e5614425ef0a2f0fa449449dd55a5f
Allan Mwenja <mwenjaallan18@gmail.com>
2025-10-08 10:08:48 +0300

    Middleware

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,14 @@ let posts = [
     {id:3, title: 'Post Three'},
 ];
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next();
+
+}
+
 // Get all posts
-router.get('/', (req, res) => {
+router.get('/', logger, (req, res) => {
     const limit = parseInt(req.query.limit);
 
     if(!isNaN(limit) && limit > 0){
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import posts from './routes/posts.js'
+import logger from './middleware/logger.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Logger middleware
+app.use(logger);
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, 'public')))
